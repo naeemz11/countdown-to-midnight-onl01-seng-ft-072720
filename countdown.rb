287879c8eb566069a6c37b78115c60d41b2de0b0
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-28 22:33:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 countdown_output= 10 
 def countdown(countdown_output)
-  while countdown_output > 0 
+  while countdown_output >= 0 
   puts "#{countdown_output} SECONDS"
 end
